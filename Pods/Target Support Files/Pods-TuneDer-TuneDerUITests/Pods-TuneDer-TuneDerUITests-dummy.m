0f2edaacc6d858d8705fe1cdd971c449742f8b38
can-merzigo <can@merzigo.com>
2020-08-12 16:15:46 +0300

    Alamofire added.

---
+++ Pods/Target Support Files/Pods-TuneDer-TuneDerUITests/Pods-TuneDer-TuneDerUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TuneDer_TuneDerUITests : NSObject
+@end
+@implementation PodsDummy_Pods_TuneDer_TuneDerUITests
+@end
---
+++ Pods/Target Support Files/Pods-TuneDer/Pods-TuneDer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TuneDer : NSObject
+@end
+@implementation PodsDummy_Pods_TuneDer
+@end
