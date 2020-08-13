43ae53a1f0995388c29ff0db2c4fbfe37b27edf7
can-merzigo <can@merzigo.com>
2020-08-13 09:44:30 +0300

    CollectionView implemented

---
+++ TuneDer/CollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  CollectionViewCell.swift
+//  TuneDer
+//
+//  Created by Huseyin Can Dayan on 13.08.2020.
+//  Copyright © 2020 Huseyin Can Dayan. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet private weak var mediaLabel: UILabel!
+    
+    func configure(with dataName: String){
+        print(dataName)
+        mediaLabel.text = dataName
+    }
+    
+}
